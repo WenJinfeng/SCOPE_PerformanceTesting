379f5d52c769ff7ce542187a5336a3152a836f8b
WenJinfengWork <jinfeng.cynthia.wen@gmail.com>
2024-07-10 16:39:54 +0800

    update

---
+++ Deployment Packages/Func40/params.py
@@ -0,0 +1,6 @@
+accessKeyId="DummyID" 
+accessKey="DummyKey"
+bucketName="bucketwendycyn1"
+LambdaRoleARN="arn:aws:iam::XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+StepRoleARN="arn:aws:iam::XXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+AccountID="1234567890"
